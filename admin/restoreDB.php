07a2c1e175f0e575d9f051a3193faf7755921126
antonioG4 <abarrera@princeton.edu>
2013-11-12 11:52:29 -0500

    restore db updates

---
+++ admin/restoreDB.php
@@ -0,0 +1,22 @@
+
+<?php include_once("../includes/locator.php"); ?>
+<?php include_once('log/login.php'); ?>
+<html>
+<head>
+<title>Firestone Locator | Database Restore</title>
+
+</head>
+<body>
+<?php 
+
+echo "<h1>Restoration Process</h1>\n";
+
+db_backup($_POST["env"]);
+
+db_restore("../sql-files/".$_POST["file"], $_POST["env"]);
+
+?>
+</body>
+
+
+</html>
