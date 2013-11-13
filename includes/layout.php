9307ba80fb6d1743485c4f8ca63155f872b22cf0
antonioG4 <abarrera@princeton.edu>
2013-11-13 11:47:13 -0500

    basic styles and functions

---
+++ includes/layout.php
@@ -0,0 +1,95 @@
+<?php
+function styles() {
+?>
+<link rel="stylesheet" href="../css/Font-Awesome/css/font-awesome.css">
+<link rel="stylesheet" href="../css/themes/pulcore/stylesheets/styles.css">
+<link rel="stylesheet" href="../css/locator.css">
+<link rel="stylesheet" href="../css/colorbox.css">
+<?php
+}
+function styles_public() {
+	?>
+<link rel="stylesheet" href="css/humanity/jquery-ui-1.8.11.custom.css" type="text/css" charset="utf-8">
+<link rel="stylesheet" href="css/pagestyle.css" type="text/css" charset="utf-8">
+<link rel="stylesheet" href="css/Font-Awesome/css/font-awesome.css">
+<link rel="stylesheet" href="css/themes/pulcore/stylesheets/styles.css">
+<link rel="stylesheet" href="css/locator.css">
+<link rel="stylesheet" href="css/colorbox.css">
+<?php
+}
+function javascript_admin() {
+?>
+	<script type="text/javascript" src="../js/swfobject.js"></script>
+	<script type="text/javascript" src="../js/jquery-1.10.2.min.js"></script>
+	<script type="text/javascript" src="../js/colorbox/jquery.colorbox-min.js"></script>
+<?php 
+}
+function javascript_public() {
+	?>
+	<script type="text/javascript" src="js/jquery-1.5.1.min.js"></script>
+	<script type="text/javascript" src="js/jquery-ui-1.8.11.custom.min.js"></script>
+	<script type="text/javascript" src="js/astar.js"></script>
+	<script type="text/javascript" src="js/raphael-min.js"></script>
+	<script type="text/javascript" src="js/locator.js"></script>
+	
+<?php 
+
+
+}
+function page_header() {
+	?>
+
+
+<h1>Firestone Locator</h1>
+
+<div id="nav">
+	<ul class="level1">
+
+		<li class="submenu">
+			<div align="center">
+				<a href="../admin/">Maintenance</a>
+				<ul class="level2">
+					<li><a href="indexExternal.php">External Locations </a></li>
+					<li><a href="indexCollections.php">Designated Locations </a></li>
+					<li><a href="indexOctavos.php">Octavos </a></li>
+					<li><a href="indexOversize.php">Oversize </a></li>
+					<li><a href="indexMessages.php">Index Card Messages </a></li>
+				</ul>
+			</div>
+		</li>
+		<li>
+			<div align="center">
+				<a href="indexDBdesign.php">Manage Maps </a>
+			</div>
+		</li>
+		<li>
+			<div align="center">
+				<a href="indexUpdateDB.php">Apply Changes</a>
+			</div>
+		</li>
+		<li>
+			<div align="center">
+				<a href="indexTest.php">Test Center</a>
+			</div>
+		</li>
+		<li>
+			<div align="center">
+				<a href="logout.php">Logout </a>
+			</div>
+		</li>
+
+
+	</ul>
+</div>
+
+
+<?php 
+}
+
+function page_footer() {
+?>
+<div id="footer">&copy; 2005-<?php echo date("Y");?>.  The Trustees of Princeton University</div>
+<?php 
+}
+?>
+
---
+++ includes/locator.php
@@ -0,0 +1,34 @@
+<?php 
+
+include_once("db_config.php");
+include_once("layout.php");
+
+
+# Escapes values for SQL by type.  Must be a better way then this.
+
+function GetSQLValueString($theValue, $theType, $theDefinedValue = "", $theNotDefinedValue = "")
+{
+	$theValue = (!get_magic_quotes_gpc()) ? addslashes($theValue) : $theValue;
+
+	switch ($theType) {
+		case "text":
+			$theValue = ($theValue != "") ? "'" . $theValue . "'" : "NULL";
+			break;
+		case "long":
+		case "int":
+			$theValue = ($theValue != "") ? intval($theValue) : "NULL";
+			break;
+		case "double":
+			$theValue = ($theValue != "") ? "'" . doubleval($theValue) . "'" : "NULL";
+			break;
+		case "date":
+			$theValue = ($theValue != "") ? "'" . $theValue . "'" : "NULL";
+			break;
+		case "defined":
+			$theValue = ($theValue != "") ? $theDefinedValue : $theNotDefinedValue;
+			break;
+	}
+	return $theValue;
+}
+
+?>
