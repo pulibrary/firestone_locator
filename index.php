867412c57441b9a041550cccaa0b452001b38417
antonioG4 <abarrera@princeton.edu>
2013-11-14 13:13:22 -0500

    html adjustment

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ include('includes/item.php');
 include('includes/extandbranch.php');
 
 ?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<!DOCTYPE html>
 <html>
 	<head>
 	    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
