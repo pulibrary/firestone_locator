8b7ffd2e611abff67734cb4631a5f8d13dce4e65
antonioG4 <abarrera@princeton.edu>
2013-11-14 14:18:12 -0500

    starting logout procedure

--- admin/log/logout.php
+++ admin/log/logout.php
@@ -1,7 +1,13 @@
 <?php
 
-	include_once('CAS.php');
-  	phpCAS::logout();
-  	header("Location: index.php");
+	include_once('phpCAS/CAS.php');
+  	// Initialize phpCAS
+	phpCAS::client(CAS_VERSION_2_0,'fed.princeton.edu',443,'cas');
+		
+	phpCAS::setNoCasServerValidation();
+	
+	phpCAS::logout();
+  	header("Location: ../index.php");
+
 
 ?>
