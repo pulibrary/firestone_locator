bb625d696f604eebcc80a4bf3886d8c0e9bb4901
antonioG4 <abarrera@princeton.edu>
2013-12-06 10:01:53 -0500

    Adding GA to the public pages

--- includes/layout.php
+++ includes/layout.php
@@ -51,7 +51,19 @@ function javascript_public() {
 	type="text/javascript" src="js/raphael-min.js"></script>
 <script
 	type="text/javascript" src="js/locator.js"></script>
+<script type="text/javascript">
 
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-15870237-13']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
 <?php 
 
 
