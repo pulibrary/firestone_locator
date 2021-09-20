286de9b3e414e6811c3bcf05ed35b958d0f116fd
Carolyn Cole <cac9@princeton.edu>
2021-09-20 10:05:31 -0400

    Adding sql-files to linked directories

--- config/deploy.rb
+++ config/deploy.rb
@@ -27,6 +27,7 @@ set :tmp_dir, '/home/deploy/tmp'
 
 # Default value for linked_dirs is []
 # append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
+append :linked_dirs, "sql-files"
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
